573c79447759f89e4e63a981dceda84cb7f27597
Colleen Bertoni <bertoni@anl.gov>
2020-10-15 18:28:45 +0000

    adding SU3

--- benchmarks/performance/run_BabelStream_JLSE_gen9.sh
+++ benchmarks/performance/run_BabelStream_JLSE_gen9.sh
@@ -6,11 +6,11 @@ cd BabelStream
 
 
 # HIPCL
-clang++ -O3 -std=c++11 -DOCL main.cpp OCLStream.cpp  -lhipcl -lOpenCL -o ocl-stream
+clang++ -O3 -std=c++11 -DHIP main.cpp HIPStream.cpp  -lhipcl -lOpenCL -o hip-stream
 
 # if what we should be measuring is the DRAM BW, then we should use 128MB as
 # the cache (eDRAM). in that case, the calculation is:
 # so for doubles, the size of the array is
 # 128000 kiB * 1024 bytes/KiB * 1 double / 8 bytes * 4 (factor for STREAM)
 # = 65536000
-timeout -k 5 60 ./ocl-stream -s 65536000
+./hip-stream -s 65536000
