c07be9785eb7cabd33443fd6b179bf73998e69c9
Colleen Bertoni <bertoni@anl.gov>
2020-12-04 07:25:04 +0000

    cleaning ERT

--- benchmarks/performance/run_ERT_JLSE.sh
+++ benchmarks/performance/run_ERT_JLSE.sh
@@ -4,16 +4,17 @@ module load hip
 module load gcc/7.4.0
 export CUDA_PATH=/soft/compilers/cuda/cuda-10.0.130
 
+cp ERT_configs/* cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0/Config
 cd cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0
 
 # HIP
-python2 ./ert --verbose=3 Config/config.hip-fp64.v100.jlse.anl.gov.01 
-python2 ./ert --verbose=3 Config/config.hip-fp32.v100.jlse.anl.gov.01 
+python ./ert --verbose=3 Config/config.hip-fp64.v100.jlse.anl.gov.01 
+python ./ert --verbose=3 Config/config.hip-fp32.v100.jlse.anl.gov.01 
 
 
 # CUDA
-python2 ./ert --verbose=3 Config/config.cuda-fp64.v100.jlse.anl.gov.01
-python2 ./ert --verbose=3 Config/config.cuda-fp32.v100.jlse.anl.gov.01
+python ./ert --verbose=3 Config/config.cuda-fp64.v100.jlse.anl.gov.01
+python ./ert --verbose=3 Config/config.cuda-fp32.v100.jlse.anl.gov.01
 
 
 
--- benchmarks/performance/run_ERT_JLSE_gen9.sh
+++ benchmarks/performance/run_ERT_JLSE_gen9.sh
@@ -3,17 +3,21 @@
 module load intel_compute_runtime
 module load hipcl
 
-cp config.hip-fp*.v100.jlse.anl.gov.01 cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0/Config
+cp ERT_configs/config.*-fp*.iris.jlse.anl.gov.01 cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0/Config
 cd cs-roofline-toolkit/Empirical_Roofline_Tool-1.1.0
 
 # HIP
-python2 ./ert --verbose=3 Config/config.hip-fp64.v100.jlse.anl.gov.01 
-python2 ./ert --verbose=3 Config/config.hip-fp32.v100.jlse.anl.gov.01 
+python ./ert --verbose=3 Config/config.hip-fp64.iris.jlse.anl.gov.01 
+python ./ert --verbose=3 Config/config.hip-fp32.iris.jlse.anl.gov.01 
 
 
-# CUDA
-python2 ./ert --verbose=3 Config/config.cuda-fp64.v100.jlse.anl.gov.01
-python2 ./ert --verbose=3 Config/config.cuda-fp32.v100.jlse.anl.gov.01
+module purge
+module load intel_compute_runtime
+
+# OCL
+python ./ert --verbose=3 Config/config.ocl-fp64.iris.jlse.anl.gov.01 
+python ./ert --verbose=3 Config/config.ocl-fp32.iris.jlse.anl.gov.01 
+
 
 
 
