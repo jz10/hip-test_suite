b536696fde9f1c988afbf6d7591154140c07fbbf
Colleen Bertoni <bertoni@anl.gov>
2020-12-10 21:42:03 +0000

    updating script

--- applications/run_cholla_JLSE.sh
+++ applications/run_cholla_JLSE.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-git checkout HIP
+git checkout CAAR
 module load hip
-LIBRARY_PATH=$LIBRARY_PATH:/home/bertoni/opt/hdf5/lib/ HDF5INCLUDE=/home/bertoni/opt/hdf5/include/  HDF5DIR=/home/bertoni/opt/hdf5 make
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bertoni/opt/hdf5/lib/ ./cholla tests/1D/Sod.txt
+export POISSON_SOLVER='-DPARIS'
+export SUFFIX='.paris-cuda'
+export TYPE=gravity
+export LIBRARY_PATH=$LIBRARY_PATH:/home/bertoni/opt/hdf5/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/bertoni/opt/hdf5/lib/
+HDF5INCLUDE=/home/bertoni/opt/hdf5/include/  HDF5DIR=/home/bertoni/opt/hdf5 make
+ ./cholla tests/1D/Sod.txt
