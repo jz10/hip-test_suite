47265a7af95ae4bc5d6cb7fae6d834b907f572d0
Brice Videau <bvideau@anl.gov>
2021-01-20 16:10:25 +0000

    Added run script for GPU-STREAM test.

---
+++ run_scripts/run_GPU-STREAM_JLSE_gen9.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module load intel_compute_runtime;
+module load hipcl;
+module load cmake;
+
+cd ../HIP-Examples/GPU-STREAM
+cmake .
+clang++ --std c++14 -g -c -o main.o main.cpp -DIMPLEMENTATION_STRING=\"HIP\"
+clang++ --std c++14 -g -c -o HIPStream.o HIPStream.cu
+clang++-link -o HIPStream main.o HIPStream.o -lhipcl -lOpenCL
