dc5a61467453ef4d9b59e5f1e4a58e6f9c7d5477
Colleen Bertoni <bertoni@anl.gov>
2021-01-31 19:52:31 +0000

    last fixes

--- run_scripts/run_gpu-burn_JLSE_gen9.sh
+++ run_scripts/run_gpu-burn_JLSE_gen9.sh
@@ -9,6 +9,7 @@ module load hipcl
 cd ../HIP-Examples/
 
 cd gpu-burn
+git reset --hard
 sed -i s/"#define GPUBURN_COMMON_H_"/"#define GPUBURN_COMMON_H_\n#include <memory>"/ common.h
 clang++ -O3 -std=c++11 -c -o gpuburn.o gpuburn.cpp  
 clang++ -O3 -std=c++11 -c -o common.o common.cpp  
@@ -16,5 +17,6 @@ clang++ -O3 -std=c++11 -c -o BurnKernel.o BurnKernel.cpp
 clang++ -O3 -std=c++11 -c -o AmdGpuMonitor.o AmdGpuMonitor.cpp  
 clang++-link -lm -lpthread -o gpuburn-hip gpuburn.o common.o BurnKernel.o AmdGpuMonitor.o   -lOpenCL -lhipcl
 # reverting
-sed -i s/"#include <memory>"/""/ common.h
 timing_check "./gpuburn-hip -t 5" "$0"
+
+git reset --hard
